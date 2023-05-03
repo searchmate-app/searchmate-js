dc4821a164cdde5c722b4cdf235edb2be6d06386
JosueRhea <josueroyale8171@gmail.com>
2023-05-03 14:45:21 -0600

    Fix styles

--- packages/searchmate-react/src/App.tsx
+++ packages/searchmate-react/src/App.tsx
@@ -1,21 +1,11 @@
 import "./App.css";
-import { searchmate } from "searchmate-js";
 import "searchmate-js/styles";
 
 function App() {
   return (
     <>
       <div id="search-container" />
-      <button
-        onClick={() => {
-          searchmate({
-            appId: "searchmate-react",
-            container: "#search-container",
-          });
-        }}
-      >
-        Clci
-      </button>
+      <button>Clci</button>
     </>
   );
 }
