194583727e47f90d1599bcf1fabe895af6106745
JosueRhea <josueroyale8171@gmail.com>
2023-05-08 11:39:34 -0600

    Add support for dark mode

--- packages/searchmate-js/src/main.ts
+++ packages/searchmate-js/src/main.ts
@@ -1,5 +1,6 @@
 import { searchmate } from "./searchmate";
 import "./style.css";
+import { createElement } from "./util";
 
 const app = document.getElementById("app");
 const button = document.createElement("button");
@@ -21,3 +22,48 @@ button.addEventListener("click", () => {
     },
   });
 });
+
+let theme: string | null;
+if (typeof localStorage !== "undefined" && localStorage.getItem("theme")) {
+  theme = localStorage.getItem("theme");
+} else if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+  theme = "dark";
+} else {
+  theme = "light";
+}
+
+if (theme !== undefined) {
+  const root = document.documentElement;
+  if (theme === "light") {
+    root.classList.remove("theme-dark");
+  } else {
+    root.classList.add("theme-dark");
+  }
+}
+
+window.addEventListener("storage", () => {
+  console.log("Hola");
+  if (localStorage.getItem("theme") !== theme) {
+    window.location.reload();
+  }
+});
+
+const themeButton = createElement("button");
+
+themeButton.textContent = "Toggle theme";
+themeButton.addEventListener("click", () => {
+  const root = document.documentElement;
+  if (theme === "light") {
+    localStorage.setItem("theme", "dark");
+    theme = "dark";
+
+    root.classList.add("theme-dark");
+  } else {
+    localStorage.setItem("theme", "light");
+    theme = "light";
+
+    root.classList.remove("theme-dark");
+  }
+});
+
+app?.appendChild(themeButton);
